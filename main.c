d669b60908539aa66b02449d47436241f7c33094
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-07-06 19:56:46 -0700

    Added a mask function

--- main.c
+++ main.c
@@ -7,35 +7,39 @@ typedef struct {
     uint8_t IN;
 } GPIO_Register;
 
+uint8_t mask(uint8_t pin) {
+    return (1u << pin);
+}
+
 int pin_isValid(uint8_t pin) {
     return pin <= 7;
 }
 
 void set_pin_output(GPIO_Register *gpio, uint8_t pin) {
     if (pin_isValid(pin))
-        gpio->DIR |= (1 << pin);
+        gpio->DIR |= mask(pin);
 }
 void set_pin_input(GPIO_Register *gpio, uint8_t pin) {
     if (pin_isValid(pin))
-        gpio->DIR &= ~(1 << pin);
+        gpio->DIR &= ~mask(pin);
 }
 
 void write_pin_high(GPIO_Register *gpio, uint8_t pin) {
     if (pin_isValid(pin))
-        gpio->OUT |= (1 << pin);
+        gpio->OUT |= mask(pin);
 }
 void write_pin_low(GPIO_Register *gpio, uint8_t pin) {
     if (pin_isValid(pin))
-        gpio->OUT &= ~(1 << pin);
+        gpio->OUT &= ~mask(pin);
 }
 
 void toggle_pin(GPIO_Register *gpio, uint8_t pin) {
     if (pin_isValid(pin))
-        gpio->OUT ^= (1 << pin);
+        gpio->OUT ^= mask(pin);
 }
 
 int read_pin(GPIO_Register *gpio, uint8_t pin) {
-    return pin_isValid(pin) ? ((gpio->IN & (1 << pin)) != 0) : -1;
+    return pin_isValid(pin) ? ((gpio->IN & mask(pin)) != 0) : -1;
 }
 
 int main(void) {
